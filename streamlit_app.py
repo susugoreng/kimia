057b5d2ce85023afb85ce5ef9cf9d80effd45dfa
susugoreng <salwa.nurrohmah@gmail.com>
2025-04-21 12:46:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("GAMES KIMIA🧪")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Selamat datang di games kimia."
 )
